c5c0d93da763a4c8816624de79fe7fc133c92588
Birkowsk <adbirkowski@gmail.com>
2021-11-09 19:05:56 +0100

    task changed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 // import Task1 from './task1-sidebarToggler/Task1';
 // import AppContext from '../Tasks/task2-tours/AppContext';
-import Task3 from './task3-reviews/Task3';
-// import Task4 from './task4-questions/Task4';
+// import Task3 from './task3-reviews/Task3';
+import Task4 from '../Tasks/task4-questions/Task4';
 // import Task5 from '../Tasks/task5-menu/Task5';
 // import Task6 from './task6-slider/Task6';
 // import Task7 from './task7-tabs/Task7';
@@ -12,7 +12,7 @@ import Task3 from './task3-reviews/Task3';
 import './main.css';
 
 function App() {
-  return < Task3 />;
+  return < Task4 />;
 }
 
 export default App;
